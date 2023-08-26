80dedc06f8b67ca8513f9a0bfe4826d4caf65d47
BrianKvin <briankvin@gmail.com>
2023-08-26 23:37:29 +0300

    revised test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,32 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
-describe("calculateNumber", function() {
-  it("should return 3 when a = 1 and b = 2", function() {
-    assert.strictEqual(calculateNumber(1, 2), 3);
+describe("calculateNumber test", function() {
+  it("should return +ve when +ve no's are passed", function() {
+    assert.equal(calculateNumber(1, 3), 4);
   });
-  it("should return 5 when a = 1 and b = 3.7", function() {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  it("should return 5 when a is 1 and b is 3.7", function() {
+    assert.equal(calculateNumber(1, 3.7), 5);
   });
-  it("should return 5 when a = 1 and b = 3.7", function() {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  it("should return 5 when a is 1.2 and b is 3.7", function() {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
   });
-  it("should return 6 when a = 1.5 and b = 3.7", function() {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  it("should return 6 when a is 1.5 and b is 3.7", function() {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+  it("should return 4 when a is 0 and b is 3.7", function() {
+    assert.equal(calculateNumber(0, 3.7), 4);
+  });
+  it("should return 0 when a is 0 and b is 0", function() {
+    assert.equal(calculateNumber(0, 0), 0);
+  });
+  it("should return 2 when a is 0.5 and b is 0.5", function() {
+    assert.equal(calculateNumber(0.5, 0.5), 2);
+  });
+  it("should return 0 when a is 0.3 and b is 0.4", function() {
+    assert.equal(calculateNumber(0.4, 0.4), 0);
+  });
+  it("should return 1 when a is 0.7 and b is 0.3", function() {
+    assert.equal(calculateNumber(0.7, 0.3), 1);
   });
 });
