f83c03c9ca36f6ec001130f98bb179a4bb385c67
BrianKvin <briankvin@gmail.com>
2023-08-26 23:30:32 +0300

    revised test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,15 +3,15 @@ const calculateNumber = require('./0-calcul');
 
 describe("calculateNumber", function() {
   it("should return 3 when a = 1 and b = 2", function() {
-    assert(calculateNumber(1, 2), 3);
+    assert.strictEqual(calculateNumber(1, 2), 3);
   });
   it("should return 5 when a = 1 and b = 3.7", function() {
-    assert(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
   it("should return 5 when a = 1 and b = 3.7", function() {
-    assert(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
   it("should return 6 when a = 1.5 and b = 3.7", function() {
-    assert(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
